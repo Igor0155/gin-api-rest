6fb112346a90377d7acad7af7631f5a72a21d4aa
Igor Gabriel <igor.gabriel@celula.net.br>
2025-05-26 15:16:11 -0300

    Finaliando curso

--- controllers/controller.go
+++ controllers/controller.go
@@ -8,13 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func ExibiTodosAluno(c *gin.Context){
-	var alunos []models.Aluno
-
-	database.DB.Find(&alunos)
-	c.JSON(200, alunos)
-}
-
 func HelloWorldName( c *gin.Context){
 	nome := c.Params.ByName("nome")
 	c.JSON(200, gin.H{
@@ -23,10 +16,42 @@ func HelloWorldName( c *gin.Context){
 
 }
 
+func ExibiTodosAluno(c *gin.Context){
+	var alunos []models.Aluno
+
+	database.DB.Find(&alunos)
+	c.JSON(200, alunos)
+}
+
+func ExibirAlunoPeloId(c *gin.Context){
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	database.DB.Find(&aluno, id)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"Not Found": "Aluno não encontrado"})
+		return
+	}
+	c.JSON(http.StatusOK, aluno)
+}
+
+func BuscaAlunoPorCpf(c *gin.Context){
+
+	var aluno models.Aluno
+	cpf := c.Params.ByName("cpf")
+
+	database.DB.Where(&models.Aluno{CPF: cpf}).First(&aluno)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"Not Found": "Aluno não encontrado"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": aluno})
+}
+
 func CriarNovoAluno(c *gin.Context){
 
 	var aluno models.Aluno
-
 	if err := c.ShouldBindJSON(&aluno); err != nil{
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -34,5 +59,33 @@ func CriarNovoAluno(c *gin.Context){
 
 	database.DB.Create(&aluno)
 	c.JSON(http.StatusOK, aluno)
-
 }
+
+func EditaAluno(c *gin.Context){
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	database.DB.Find(&aluno, id)
+
+	if err := c.ShouldBindJSON(&aluno); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	database.DB.Model(&aluno).UpdateColumns(aluno)
+	c.JSON(http.StatusOK, aluno)
+}
+
+func DeletaAlunoPorId(c *gin.Context){
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	database.DB.Find(&aluno, id)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"Not Found": "Aluno não encontrado"})
+		return
+	}
+	database.DB.Delete(&aluno, id)
+	c.JSON(http.StatusOK, gin.H{"data": "Aluno: "+ aluno.Nome +" deletado"})
+}
+
+
--- routes/route.go
+++ routes/route.go
@@ -7,8 +7,12 @@ import (
 
 func HandleRequest() {
 	r := gin.Default()
-	r.GET("/alunos", controllers.ExibiTodosAluno)
 	r.GET(":nome", controllers.HelloWorldName )
+	r.GET("/alunos", controllers.ExibiTodosAluno)
+	r.GET("/alunos/:id", controllers.ExibirAlunoPeloId)
+	r.GET("alunos/cpf/:cpf", controllers.BuscaAlunoPorCpf)
 	r.POST("/alunos", controllers.CriarNovoAluno)
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.DELETE("/alunos/:id", controllers.DeletaAlunoPorId)
 	r.Run()
 }
